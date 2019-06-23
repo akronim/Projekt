f23c54ff58327cdfd718eba586482aa180953a6f
Marinko Mišković <marinko.miskovic.hr@gmail.com>
2019-06-24 00:41:33 +0200

    Finished login process

--- src/js/base/jQueryMethods.js
+++ src/js/base/jQueryMethods.js
@@ -12,3 +12,18 @@
     return data;
   };
 })(jQuery);
+
+let getLoader = function() {
+  let html = "";
+  html += '<div class="loader">';
+  html += '<img src="/assets/images/loader.svg">';
+  html += "</div>";
+
+  return html;
+};
+
+let setAjaxMessage = function(message) {
+  let html = '<div class="message">' + message + "</div>";
+
+  return html;
+};
